e3e70b49f8a08337cf46bd06f51c272cb4de7583
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-03-18 19:47:35 +0530

    feat: Implement ExplorePage component

--- frontend/src/pages/Explorepage.jsx
+++ frontend/src/pages/Explorepage.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 
-const Explorepage = () => {
+const ExplorePage = () => {
   return (
-    <div>Explorepage</div>
-  )
-}
+    <div className="px-4">
+      <div className="max-w-2xl p-4 mx-auto rounded-md bg-glass">
+        <h1 className="text-xl font-bold text-center">
+          Explore Popular Repositories
+        </h1>
+        <div className="flex flex-wrap justify-center gap-2 my-2">
+          <img
+            src="/javascript.svg"
+            alt="JavaScript"
+            className="cursor-pointer h-11 sm:h-20"
+          />
+          <img
+            src="/typescript.svg"
+            alt="TypeScript logo"
+            className="cursor-pointer h-11 sm:h-20"
+          />
+          <img
+            src="/c++.svg"
+            alt="C++ logo"
+            className="cursor-pointer h-11 sm:h-20"
+          />
+          <img
+            src="/python.svg"
+            alt="Python logo"
+            className="cursor-pointer h-11 sm:h-20"
+          />
+          <img
+            src="/java.svg"
+            alt="Java logo"
+            className="cursor-pointer h-11 sm:h-20"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Explorepage
+export default ExplorePage
