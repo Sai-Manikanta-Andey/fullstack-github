c678eedeac3ee6c7035318e1ede4a6a5559d7291
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-03-19 11:06:48 +0530

    feat:HomePage implement

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Search from '../components/Search'
+import SortRepos from '../components/SortRepos'
+import ProfileInfo from "../components/ProfileInfo";
+import Repo from "../components/Repo";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div className='m-4 '>
+      <Search/>
+      <SortRepos/>
+      <div className='flex flex-col items-start justify-center gap-4 lg:flex-row'>
+        <ProfileInfo/>
+        <Repo/>
+        <Repo/>
+        <Repo/>
+        <Repo/>
+      </div>
+    </div>
   )
 }
 
