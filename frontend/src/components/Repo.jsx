b849d3a3d026bfcc6519924aa1588830ad38543e
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-03-20 08:47:13 +0530

    feat(frontend): Completed frontend development

--- frontend/src/components/Repo.jsx
+++ frontend/src/components/Repo.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 import { FaCodeBranch, FaCopy, FaRegStar } from "react-icons/fa";
 import { FaCodeFork } from "react-icons/fa6";
 
 const Repo = () => {
   return (
-    <li className="mb-10 ms-7">
+    <li className="mb-10 ms-7 ">
       <span
         className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white"
       >
@@ -56,4 +54,4 @@ const Repo = () => {
     </li>
   );
 };
-export default Repo
+export default Repo;
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import Search from '../components/Search'
 import SortRepos from '../components/SortRepos'
 import ProfileInfo from "../components/ProfileInfo";
-import Repo from "../components/Repo";
+import Repos from "../components/Repos";
 
 const Homepage = () => {
   return (
     <div className='m-4 '>
       <Search/>
       <SortRepos/>
-      <div className='flex flex-col items-start justify-center gap-4 lg:flex-row'>
+      <div className='flex flex-col items-start justify-center gap-4 md:flex-row'>
         <ProfileInfo/>
-        <Repo/>
-        <Repo/>
-        <Repo/>
-        <Repo/>
+        <Repos/>
+        
+        
       </div>
     </div>
   )
