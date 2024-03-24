e0cde07aa9f9152901294b8f68547b2b4c6aea7d
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-03-24 10:26:25 +0530

    feat: functionality for sorting repos implemented

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,25 +1,21 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import Search from '../components/Search'
-import SortRepos from '../components/SortRepos'
+import React, { useCallback, useEffect, useState } from "react";
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
 import ProfileInfo from "../components/ProfileInfo";
 import Repos from "../components/Repos";
-import toast from 'react-hot-toast';
-import Spinner from "../components/Spinner"
+import toast from "react-hot-toast";
+import Spinner from "../components/Spinner";
 
 const Homepage = () => {
-  const [user,setUser]=useState(null)
-  const [repos,setRepos]= useState([])
-  const [loading,setLoading] =useState(false)
-  const [sortType,setsortType] = useState("recent")
-  
+  const [user, setUser] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [sortType, setsortType] = useState("recent");
 
-  
   const getUserProfile = async (username = "Sai-Manikanta-Andey") => {
     setLoading(true);
     try {
-      const res = await fetch(
-        `https://api.github.com/users/${username}`
-      );
+      const res = await fetch(`https://api.github.com/users/${username}`);
       const userData = await res.json();
       setUser(userData);
 
@@ -31,40 +27,55 @@ const Homepage = () => {
       setRepos(reposData);
       console.log("user:", userData);
       console.log("repos:", reposData);
-      return {userData,reposData}
+      return { userData, reposData };
     } catch (error) {
       toast.error("Failed to fetch");
     } finally {
       setLoading(false);
     }
   };
-  
-  useEffect(()=>{
-    getUserProfile()
-  },[])
-  
-  const onSearch = async (e,username)=>{
-    e.preventDefault()
-    setLoading(true)
-    setRepos([])
-    setUser(null)
-   const {userData,reposData}=  await getUserProfile(username)
-   setUser(userData)
-   setRepos(reposData)
-  }
-  
+
+  useEffect(() => {
+    getUserProfile();
+  }, []);
+
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+    setLoading(true);
+    setRepos([]);
+    setUser(null);
+    const { userData, reposData } = await getUserProfile(username);
+    setUser(userData);
+    setRepos(reposData);
+    setLoading(false);
+  };
+
+  const onSort = (sortType) => {
+    if (sortType === "recent") {
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    } else if (sortType === "stars") {
+      repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+    } else if (sortType === "forks") {
+      repos.sort((a, b) => b.forks_count - a.forks_count);
+    }
+    setsortType(sortType);
+    setRepos([...repos]);
+  };
+
   return (
     <div className="m-4 ">
       {user?.message === "Not Found" ? (
-        <p className='font-bold text-center'>User not found</p>
+        <p className="font-bold text-center">User not found</p>
       ) : (
         <>
           {" "}
           <Search onSearch={onSearch} />
-          <SortRepos />
+          {Repos.length > 0 && (
+            <SortRepos sortType={sortType} onSort={onSort} />
+          )}
           <div className="flex flex-col items-start justify-center gap-4 md:flex-row">
             {user && !loading && <ProfileInfo userProfile={user} />}
-            { !loading && <Repos repos={repos} />}
+            {!loading && <Repos repos={repos} />}
 
             {loading && <Spinner />}
           </div>
@@ -72,6 +83,6 @@ const Homepage = () => {
       )}
     </div>
   );
-}
+};
 
-export default Homepage
+export default Homepage;
